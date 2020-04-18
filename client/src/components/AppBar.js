3df94b059f1b07c4db9cd38bde4b1e82fb327c86
colemanimhoff <colemanimhoff@gmail.com>
2020-04-18 15:12:08 -0600

    some styling

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,21 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 
 const AppBar = () => {
-    const [ user, setUser ] = useState({ id: '', username: '' })
-    const [ isLoggedIn, setIsLoggedIn ] = useState(false)
-    const handleLogin = () => {
-        setUser({ id: '1', user: 'colemanimhoff' })  // hardcoded for now
-        setIsLoggedIn(true)
-    }
-    const handleLogout = () => {
-        setUser({ id: '', username: '' })
-        setIsLoggedIn(false)
-    }
-
-    console.log(user)
-
     const path = useLocation().pathname
 
     return (
@@ -26,25 +13,15 @@ const AppBar = () => {
             <Link className={path === '/albums' ? 'item active' : 'item'} to="/albums">
                 Albums
             </Link>
-            { isLoggedIn ? (
                 <div className="right menu">
-                    <Link className={path === 'albums/new' ? 'item active' : 'item'} to="/login">
-                        Add Albums
-                    </Link>
-                    <Link className={path === '/logout' ? 'item active' : 'item'} onClick={handleLogout} to="/signup">
-                        Log Out
-                    </Link>
-                </div>
-            ) : (
-                <div className="right menu">
-                    <Link className={path === '/login' ? 'item active' : 'item'} onClick={handleLogin} to="/login">
+                    <Link className={path === '/login' ? 'item active' : 'item'} to="/login">
                         Login
                     </Link>
                     <Link className={path === '/signup' ? 'item active' : 'item'} to="/signup">
                         Signup
                     </Link>
                 </div>
-            ) }
+            )
         </div>
     )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
 import AppBar from '../components/AppBar'
+import { Card, Image } from 'semantic-ui-react'
 
 const Home = () => {
     return (
-        <div>
+        <>
             <AppBar />
-            <div className="ui container">
-            </div>
-        </div>
+                <div className="ui container">
+                    <div className="ui segment">
+                        <h1 className="main-title">
+                            <div>
+                                <span>YOUR </span>
+                                <span>BAND </span>
+                            </div>
+                            <span>S</span>
+                            <span className="flickering">U</span>
+                            <span>CKS</span>
+                        </h1>
+                        <Card.Group>
+                            <Card>
+                                <Image src="https://img.discogs.com/KXAqKm3ssz9NEa1O3l50Yl7uqTY=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1230305-1260858035.jpeg.jpg" wrapped ui={false}></Image>
+                            </Card>
+                        </Card.Group>
+                    </div>
+                </div>
+        </>
     )
 }
 
